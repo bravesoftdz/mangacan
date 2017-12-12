abf5ec4dd561f7e1946b11fce296fbf00643ee2b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-12-13 03:06:33 +0700

    hapus daftar judal dan gambar lengkap dari project

--- mangacan.dpr
+++ mangacan.dpr
@@ -3,16 +3,13 @@ program mangacan;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  GambarLengkap in 'GambarLengkap.pas' {FrmLengkap},
   HtmlParser in 'HtmlParser.pas',
-  DaftarJudul in 'DaftarJudul.pas' {FrmDaftar},
   SinglePage in 'SinglePage.pas' {FrmSingle};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-//  Application.CreateForm(TFrmLengkap, FrmLengkap);
   Application.CreateForm(TFrmSingle, FrmSingle);
   Application.Run;
 end.
