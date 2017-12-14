7aadfce391818f9e6deeb737418fdf789193b075
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-12-14 19:32:20 +0700

    common unit

--- mangacan.dpr
+++ mangacan.dpr
@@ -5,12 +5,14 @@ uses
   FMX.Forms,
   HtmlParser in 'HtmlParser.pas',
   SinglePage in 'SinglePage.pas' {FrmSingle},
-  TitlePage in 'TitlePage.pas' {FrmTitle};
+  TitlePage in 'TitlePage.pas' {FrmTitle},
+  Commons in 'Commons.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TFrmTitle, FrmTitle);
+  Application.CreateForm(TFrmSingle, FrmSingle);
   Application.Run;
 end.
